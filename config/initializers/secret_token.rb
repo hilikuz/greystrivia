5178b13b13d02e1dd0c2ac9ddac61fe387140395
Archie Semillano <archie.semillano@gmail.com>
2011-03-28 18:52:23 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trivia::Application.config.secret_token = 'a36e3809b388d0120bfbadc79b0b77bdb379fd96854f1b5c4173bf83ec23c4b4767388112da4f54124103686b6ccc71f5b9b171dc00645c458ca24104b0760cf'
